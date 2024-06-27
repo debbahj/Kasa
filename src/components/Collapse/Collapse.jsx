872799e8e6bf8bf0437bb8025323f5bd230e9184
debbahj <debbah.pj@gmail.com>
2024-06-27 21:31:58 +0200

    refonte collpase avec balise <details> + <summary>

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -1,30 +1,17 @@
-import { useState } from 'react'
 import PropTypes from 'prop-types'
 import './Collapse.css'
 
 const Collapse = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  const handleClick = () => {
-    setIsOpen(!isOpen)
-  }
-
   return (
-    <div className="collapse">
-      <button className="collapse__button" onClick={handleClick} type="button">
-        <p className="collapse__title">{title}</p>
-        <i
-          className={`collapse__icon fas fa-chevron-up ${
-            isOpen ? 'anim-rotate' : ''
-          }`}
-        ></i>
-      </button>
-      {isOpen && (
-        <div className={`collapse__content ${isOpen ? 'anim-open' : ''}`}>
-          <p>{content}</p>
-        </div>
-      )}
-    </div>
+    <details className="collapse">
+      <summary className="collapse__title">
+        {title}
+        <i className="collapse__icon fas fa-chevron-up"></i>
+      </summary>
+      <div className="collapse__content">
+        <p>{content}</p>
+      </div>
+    </details>
   )
 }
 
